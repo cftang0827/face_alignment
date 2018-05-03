bdd6356b561f9e64c45ce7bd0ae45b73d29fe584
Paul <paul-tang@astra.cloud>
2018-05-03 18:44:39 +0800

    [Modify] Change to multi image mode and do multi image alignment in one time

--- api.py
+++ api.py
@@ -1,8 +1,8 @@
 import face_recognition as fr
-from skimage.transform import warp, SimilarityTransform
+from skimage.transform import warp, SimilarityTransform, resize
 import numpy as np
 
-def face_alignment(img, scale=0.9):
+def face_alignment(img, scale=0.9, face_size=(224,224)):
     '''
     face alignment API for single image, get the landmark of eyes and nose and do warpaffine transformation
     :param face_img: single image that including face, I recommend to use dlib frontal face detector
@@ -10,20 +10,20 @@ def face_alignment(img, scale=0.9):
 
     :return: an aligned single face image
     '''
-    # h, w, c = face_img.shape
-    # face_loc_whole_img = [(0, w - 1, h - 1, 0)]
-    face_land = fr.face_landmarks(img)
-    if len(face_land) != 1:
-        print('There are no face of more than one face in the picture, error..')
-        raise EOFError
-    left_eye_center = _find_center_pt(face_land[0]['left_eye'])
-    right_eye_center = _find_center_pt(face_land[0]['right_eye'])
-    trotate = _get_rotation_matrix(left_eye_center, right_eye_center, img, scale=scale)
-    warped = warp(img, trotate)
-    warped = (warped*255).astype(np.uint8)
-    face_loc = fr.face_locations(warped)[0]
-    output_img = _crop_face(warped, face_loc)
-    # warped = warp(warped, tform)
+    h, w, c = img.shape
+    output_img = list()
+    face_loc_list = fr.face_locations(img)
+    for face_loc in face_loc_list:
+        face_img = _crop_face(img, face_loc, padding_size=int((face_loc[2] - face_loc[0])*0.5))
+        face_land = fr.face_landmarks(face_img)
+
+        left_eye_center = _find_center_pt(face_land[0]['left_eye'])
+        right_eye_center = _find_center_pt(face_land[0]['right_eye'])
+        trotate = _get_rotation_matrix(left_eye_center, right_eye_center, img, scale=scale)
+        warped = warp(face_img, trotate)
+        warped = (warped*255).astype(np.uint8)
+        new_face_loc = fr.face_locations(warped)[0]
+        output_img.append(resize(_crop_face(warped, new_face_loc), face_size))
 
     return output_img
 
@@ -66,9 +66,23 @@ def _dist_nose_tip_center_and_img_center(nose_pt, img_shape):
     img_center = (x_img//2, y_img//2)
     return ((img_center[0] - nose_pt[0]), -(img_center[1] - nose_pt[1]))
 
-def _crop_face(img, face_loc):
+def _crop_face(img, face_loc, padding_size=0):
     '''
     crop face into small image, face only, but the size is not the same
     '''
-    img_crop = img[face_loc[0]:face_loc[2], face_loc[3]:face_loc[1]]
+    h, w, c = img.shape
+    top = face_loc[0] - padding_size
+    right = face_loc[1] + padding_size
+    down = face_loc[2] + padding_size
+    left = face_loc[3] - padding_size
+
+    if top < 0:
+        top = 0
+    if right > w - 1:
+        right = w - 1
+    if down > h - 1:
+        down = h - 1
+    if left < 0:
+        left = 0
+    img_crop = img[top:down, left:right]
     return img_crop
