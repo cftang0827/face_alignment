ae0fac4aa1e5658aa74027ec28eab876606c505e
Paul <paul-tang@astra.cloud>
2018-05-24 01:57:57 +0800

    [Modify] From skimage to opencv warpaffine, beside, use small face face_locations to accelerate

--- api.py
+++ api.py
@@ -1,6 +1,6 @@
 import face_recognition as fr
-from skimage.transform import warp, SimilarityTransform, resize
 import numpy as np
+import cv2
 
 def face_alignment(img, scale=0.9, face_size=(224,224)):
     '''
@@ -12,21 +12,22 @@ def face_alignment(img, scale=0.9, face_size=(224,224)):
     '''
     h, w, c = img.shape
     output_img = list()
-    face_loc_list = fr.face_locations(img)
+    face_loc_list = _face_locations_small(img)
     for face_loc in face_loc_list:
         face_img = _crop_face(img, face_loc, padding_size=int((face_loc[2] - face_loc[0])*0.5))
-        face_land = fr.face_landmarks(face_img)
+        face_loc_small_img = _face_locations_small(face_img)
+        face_land = fr.face_landmarks(face_img, face_loc_small_img)
         if len(face_land) == 0:
             return []
         left_eye_center = _find_center_pt(face_land[0]['left_eye'])
         right_eye_center = _find_center_pt(face_land[0]['right_eye'])
-        trotate = _get_rotation_matrix(left_eye_center, right_eye_center, img, scale=scale)
-        warped = warp(face_img, trotate)
-        warped = (warped*255).astype(np.uint8)
+        nose_center = _find_center_pt(face_land[0]['nose_tip'])
+        trotate = _get_rotation_matrix(left_eye_center, right_eye_center, nose_center, img, scale=scale)
+        warped = cv2.warpAffine(face_img, trotate, (w, h))
         new_face_loc = fr.face_locations(warped)
         if len(new_face_loc) == 0:
             return []
-        output_img.append(resize(_crop_face(warped, new_face_loc[0]), face_size))
+        output_img.append(cv2.resize(_crop_face(warped, new_face_loc[0]), face_size))
 
     return output_img
 
@@ -51,15 +52,16 @@ def _angle_between_2_pt(p1, p2):
     x1, y1 = p1
     x2, y2 = p2
     tan_angle = (y2 - y1) / (x2 - x1)
-    return np.deg2rad(np.degrees(np.arctan(tan_angle)))
+    return (np.degrees(np.arctan(tan_angle)))
 
-def _get_rotation_matrix(left_eye_pt, right_eye_pt, face_img, scale=0.9):
+def _get_rotation_matrix(left_eye_pt, right_eye_pt, nose_center, face_img, scale):
     '''
     to get a rotation matrix by using skimage, including rotate angle, transformation distance and the scale factor
     '''
     eye_angle = _angle_between_2_pt(left_eye_pt, right_eye_pt)
-    trotate = SimilarityTransform(rotation=eye_angle, scale=scale)
-    return trotate
+    M = cv2.getRotationMatrix2D((nose_center[0]/2, nose_center[1]/2), eye_angle, scale )
+
+    return M
 
 def _dist_nose_tip_center_and_img_center(nose_pt, img_shape):
     '''
@@ -89,3 +91,25 @@ def _crop_face(img, face_loc, padding_size=0):
         left = 0
     img_crop = img[top:down, left:right]
     return img_crop
+
+def _face_locations_raw(img, scale):
+#     img_scale = (tf.resize(img, (img.shape[0]//scale, img.shape[1]//scale)) * 255).astype(np.uint8)
+    h, w, c = img.shape
+    img_scale = cv2.resize(img, (int(img.shape[1]//scale), int(img.shape[0]//scale)))
+    face_loc_small = fr.face_locations(img_scale)
+    face_loc = []
+    for ff in face_loc_small:
+        tmp = [pt*scale for pt in ff]
+        if tmp[1] >= w:
+            tmp[1] = w
+        if tmp[2] >= h:
+            tmp[2] = h
+        face_loc.append(tmp)
+    return face_loc
+
+def _face_locations_small(img):
+    for scale in [16, 8, 4, 2, 1]:
+        face_loc = _face_locations_raw(img, scale)
+        if face_loc != []:
+            return face_loc
+    return []
